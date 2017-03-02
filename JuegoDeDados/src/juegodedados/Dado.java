9bd3fe3c1eb5904ee4ee9b2850ea6b4067786b54
labing506 <labing506@labing50620.udistrital.edu.co>
2017-03-01 22:56:40 -0500

    Iteracion numero uno

---
+++ JuegoDeDados/src/juegodedados/Dado.java
@@ -0,0 +1,22 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package juegodedados;
+
+import java.util.Random;
+
+/**
+ *
+ * @author labing506
+ */
+public class Dado {
+    
+    int valor;
+    
+    Random aleatorio= new Random();
+   public void lanzar(){
+        valor=aleatorio.nextInt(0-6+1)+6;
+    }
+}
